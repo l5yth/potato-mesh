ade4187c085d3d29c68901c4f78e4b9c94eba6bb
l5y <220195275+l5yth@users.noreply.github.com>
2026-01-10 13:01:54 +0100

    Add Apache headers to missing sources

--- app/debug.sh
+++ app/debug.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+# Copyright © 2025-26 l5yth & contributors
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 export GIT_TAG="$(git describe --tags --abbrev=0)"
 export GIT_COMMITS="$(git rev-list --count ${GIT_TAG}..HEAD)"
 export GIT_SHA="$(git rev-parse --short=9 HEAD)"
@@ -12,4 +25,3 @@ flutter run \
   --dart-define=GIT_SHA="${GIT_SHA}" \
   --dart-define=GIT_DIRTY="${GIT_DIRTY}" \
   --device-id 38151FDJH00D4C
-
